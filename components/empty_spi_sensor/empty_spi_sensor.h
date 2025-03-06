0b60b3c7ef266937d7ce285be442a30ae6317255
ficueu <strona@ficu.eu>
2025-03-06 15:17:03 +0100

    Update empty_spi_sensor.h

--- components/empty_spi_sensor/empty_spi_sensor.h
+++ components/empty_spi_sensor/empty_spi_sensor.h
@@ -8,9 +8,12 @@ namespace esphome
   namespace empty_spi_sensor
   {
     class EmptySPISensor : public sensor::Sensor,
-                           public PollingComponent,
+                           public PollingComponent
     {
     public:
+      void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
+      void set_humidity_sensor(sensor::Sensor *humidity_sensor) { humidity_sensor_ = humidity_sensor; }
+      
       void setup() override;
       void update() override;
       void loop() override;
