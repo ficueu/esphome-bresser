b9d67ca530a9557ebcb90ebd7ace526de98514fb
ficueu <strona@ficu.eu>
2025-03-06 13:35:33 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -5,12 +5,12 @@ from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
 
 DEPENDENCIES = ['spi']
 
-empty_spi_sensor_ns = cg.esphome_ns.namespace('empty_spi_sensor')
-EmptySPISensor = empty_spi_sensor_ns.class_('EmptySPISensor', cg.PollingComponent,
+bresser = cg.esphome_ns.namespace('bresser')
+Bresser = bresser_ns.class_('Bresser', cg.PollingComponent,
                                   spi.SPIDevice)
 
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
-    cv.GenerateID(): cv.declare_id(EmptySPISensor),
+    cv.GenerateID(): cv.declare_id(Bresser),
 }).extend(cv.polling_component_schema('60s')).extend(spi.spi_device_schema())
 
 
