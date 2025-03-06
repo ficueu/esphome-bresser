4d8a7844a37ea8a551370162bfeffe9913af6177
ficueu <strona@ficu.eu>
2025-03-06 15:07:01 +0100

    update

--- components/empty_spi_sensor/sensor.py
+++ components/empty_spi_sensor/sensor.py
@@ -10,19 +10,35 @@ EmptySPISensor = empty_spi_sensor_ns.class_(
     "EmptySPISensor", cg.PollingComponent, spi.SPIDevice
 )
 
-CONFIG_SCHEMA = (
-    sensor.sensor_schema(
-        EmptySPISensor,
-        unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=1,
-    )
-    .extend(cv.polling_component_schema("60s"))
-    .extend(spi.spi_device_schema())
-)
+CONF_SENSOR1 = "temperature"
+CONF_SENSOR2 = "humidity"
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(EmptySPISensor),
+        cv.Optional(CONF_SENSOR1): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
+        ),
+        cv.Optional(CONF_SENSOR2): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
+        ),
+    }
+).extend(cv.polling_component_schema("60s"))
 
 
 async def to_code(config):
-    var = await sensor.new_sensor(config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await spi.register_spi_device(var, config)
+
+    if sensor_1_config := config.get(CONF_SENSOR1):
+        sens = await sensor.new_sensor(sensor_1_config)
+        cg.add(var.set_sensor1(sens))
+
+    if sensor_2_config := config.get(CONF_SENSOR2):
+        sens = await sensor.new_sensor(sensor_2_config)
+        cg.add(var.set_sensor2(sens))
+
+    if sensor_3_config := config.get(CONF_SENSOR3):
+        sens = await sensor.new_sensor(sensor_3_config)
+        cg.add(var.set_sensor3(sens))
